9185bfba6b61e841acab87a5931b5bf128552df8
JuanPe <juanpedroboda@gmail.com>
2025-06-07 02:46:08 -0300

    Juego_El_Ahorcado

---
+++ repo_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void utf8();
+
+int main() {
+    utf_8();
+
+    return 0;
+}
+
+// Función para compatibilizar caracteres ASCII.
+void utf_8() {
+#ifdef _WIN32
+#include <windows.h>
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+}
